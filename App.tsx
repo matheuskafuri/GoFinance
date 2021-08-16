3b5cfd92d96b5b28f09498718eca06adce18c26e
Matheus Kafuri <matheus10.rk@gmail.com>
2021-08-16 13:54:25 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
+import { AppRoutes } from './src/routes/app.routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+  
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+
+  return ( 
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <AppRoutes />     
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
